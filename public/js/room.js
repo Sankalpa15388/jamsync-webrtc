8087bfe7dc6916210a765204c09e3008ae7ef928
Sankalpa <kavindusankalpa465@gmail.com>
2026-03-09 14:48:50 +0530

    added mac ip to socket connection

--- public/js/room.js
+++ public/js/room.js
@@ -66,7 +66,7 @@
   document.title = `JamSync | Room ${roomId}`;
 
   // --- Socket.IO Connection ---
-  const socket = io();
+  const socket = io("http://192.168.8.159:3000");
 
   // --- Get Local Media ---
   async function initLocalStream() {
